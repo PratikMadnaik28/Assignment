03988eac6fdc7107f6ed973bb5d1d917fdb018b9
PratikMadnaik28 <114127094+PratikMadnaik28@users.noreply.github.com>
2026-02-21 18:19:49 -0500

    Add files via upload

---
+++ assignment1.py
@@ -0,0 +1,13 @@
+#code1
+#print("Python's the best!")
+
+#code2
+#print('The cat said "meow."')
+
+#code3
+name = input("What is your name? ")
+age = int(input("What is your age? "))
+income = float(input("What is your income? "))
+print("Name: ",name)
+print("Age: ",age)
+print("Income: ",income)
